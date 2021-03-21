d3277e3d9c34440269663289ab34062bec4c2369
Marcel <marcelvarga201@gmail.com>
2021-03-21 20:11:26 +0000

    Added AddBooks

---
+++ Bookshop/AddBooks.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Bookshop
+{
+    partial class AddBooks
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.BooksList = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.quantity = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.StockCount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(64, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(455, 39);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Add Books to the Database";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(242, 29);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Change Book Stock";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(335, 190);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 25);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Choose book:";
+            // 
+            // BooksList
+            // 
+            this.BooksList.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.BooksList.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.BooksList.FormattingEnabled = true;
+            this.BooksList.Location = new System.Drawing.Point(166, 146);
+            this.BooksList.Name = "BooksList";
+            this.BooksList.Size = new System.Drawing.Size(226, 24);
+            this.BooksList.TabIndex = 12;
+            this.BooksList.SelectedIndexChanged += new System.EventHandler(this.BooksList_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(160, 25);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Choose quantity:";
+            // 
+            // quantity
+            // 
+            this.quantity.Location = new System.Drawing.Point(187, 190);
+            this.quantity.Name = "quantity";
+            this.quantity.Size = new System.Drawing.Size(126, 22);
+            this.quantity.TabIndex = 16;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(426, 190);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Remove";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(408, 149);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 17);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Stock: ";
+            // 
+            // StockCount
+            // 
+            this.StockCount.AutoSize = true;
+            this.StockCount.Location = new System.Drawing.Point(465, 149);
+            this.StockCount.Name = "StockCount";
+            this.StockCount.Size = new System.Drawing.Size(83, 17);
+            this.StockCount.TabIndex = 19;
+            this.StockCount.Text = "Select Book";
+            // 
+            // AddBooks
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 397);
+            this.Controls.Add(this.StockCount);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.quantity);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.BooksList);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "AddBooks";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox BooksList;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox quantity;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label StockCount;
+    }
+}
---
+++ Bookshop/AddBooks.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Bookshop
+{
+    public partial class AddBooks : Form
+    {
+        public AddBooks()
+        {
+            InitializeComponent();
+            this.Text = "Add Books";
+            UpdateBooksList();
+        }
+        private void UpdateBooksList()
+        {
+            //Create connection and store Table Data into a variable
+            string conString = @"Data Source=.\SQLEXPRESS;AttachDbFilename=" + Application.StartupPath + "\\Database.MDF;Integrated Security=True;User Instance=True";
+            string sql = @"Select Title FROM Books ORDER BY Title";
+
+            SqlConnection con = new SqlConnection(conString);
+            con.Open();
+            SqlDataAdapter da = new SqlDataAdapter(sql, con);
+            con.Close();
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            // Populate BookList with book titles from the database
+            foreach (DataRow Row in dt.Rows)
+                BooksList.Items.Add(Row["Title"]);
+        }
+
+        private void BooksList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Update stock label to match selected BookList item
+            string conString = @"Data Source=.\SQLEXPRESS;AttachDbFilename=" + Application.StartupPath + "\\Database.MDF;Integrated Security=True;User Instance=True";
+            string sql = @"Select Stock FROM Books WHERE Title = '" + BooksList.SelectedItem + "'";
+            SqlConnection con = new SqlConnection(conString);
+            con.Open();
+            SqlCommand cmd = new SqlCommand(sql, con);
+            StockCount.Text = cmd.ExecuteScalar().ToString();
+            con.Close();
+        }
+        private void button_Click(bool add)
+        {
+            if (BooksList.SelectedIndex == -1) return;
+            // Find the value to which the Stock should be updated
+            int currentStock = Int16.Parse(StockCount.Text);
+            int updateStockBy = 0;
+            if (quantity.Text != "")
+                updateStockBy = Int16.Parse(quantity.Text);
+            if (add == true) currentStock = currentStock + updateStockBy;
+            else if(currentStock >= updateStockBy) currentStock = currentStock - updateStockBy;
+                else currentStock = 0;
+
+            // Change Book Stock value in the Database
+            string SelectedBook = BooksList.GetItemText(BooksList.SelectedItem);
+            string conString = @"Data Source=.\SQLEXPRESS;AttachDbFilename=" + Application.StartupPath + "\\Database.mdf;Integrated Security=True;User Instance=True";
+            string sql = @"UPDATE Books SET Stock ='" + currentStock.ToString() + "' WHERE Title = '" + SelectedBook + "'";
+            SqlConnection con = new SqlConnection(conString); con.Open();
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.ExecuteScalar();
+            con.Close();
+
+            StockCount.Text = currentStock.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // true == increase Stock value
+            button_Click(true);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // false == decrease Stock value
+            button_Click(false);
+        }
+
+    }
+}
